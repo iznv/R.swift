27897b0f2679fe6d40fbdfd530749e9e78441745
Ivan Zinovev <zinovyev-ivan@yandex.ru>
2023-02-22 19:50:26 +0300

    Add 7.2.4

--- Package.swift
+++ Package.swift
@@ -6,23 +6,10 @@ import PackageDescription
 let package = Package(
     name: "Rswift",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Rswift",
-            targets: ["Rswift"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "RswiftLibrary",
+                 targets: ["RswiftResources"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Rswift",
-            dependencies: []),
-        .testTarget(
-            name: "RswiftTests",
-            dependencies: ["Rswift"]),
+        .target(name: "RswiftResources")
     ]
 )
--- Sources/Rswift/Rswift.swift
+++
@@ -1,6 +0,0 @@
-public struct Rswift {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
